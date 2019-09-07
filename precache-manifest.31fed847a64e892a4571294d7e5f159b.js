c2173ff953b742f54ee82deff7678eecd34a9333
gitSushi <lossushi37@gmail.com>
2019-09-07 18:03:47 +0200

    Updates

---
+++ precache-manifest.31fed847a64e892a4571294d7e5f159b.js
@@ -0,0 +1,34 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "57b4edf23f8e29bc8999792d1f0090e7",
+    "url": "/portfolio-2019/index.html"
+  },
+  {
+    "revision": "d16a63fe1c116ada75b9",
+    "url": "/portfolio-2019/static/css/2.266e55a5.chunk.css"
+  },
+  {
+    "revision": "86c982d2c457b6004054",
+    "url": "/portfolio-2019/static/css/main.3c610529.chunk.css"
+  },
+  {
+    "revision": "d16a63fe1c116ada75b9",
+    "url": "/portfolio-2019/static/js/2.18280c97.chunk.js"
+  },
+  {
+    "revision": "86c982d2c457b6004054",
+    "url": "/portfolio-2019/static/js/main.daeee3b7.chunk.js"
+  },
+  {
+    "revision": "a5f019aee1f072da8bc1",
+    "url": "/portfolio-2019/static/js/runtime~main.a2571eea.js"
+  },
+  {
+    "revision": "32740a819af8a573fb719826b1a949fe",
+    "url": "/portfolio-2019/static/media/Screenshot_2019-08-29 Hangman Game(1).32740a81.png"
+  },
+  {
+    "revision": "8ea398bc4c999da924cce1e7adaf6256",
+    "url": "/portfolio-2019/static/media/Screenshot_2019-08-29 Quote Generator(1).8ea398bc.png"
+  }
+]);
